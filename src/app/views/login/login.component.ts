50045a4594e7dc35aa214ffe6289499cbf8a0038
DIVYANSHU TIWARI <divyanshu.tiwari@mobiliiya.com>
2019-03-05 13:46:32 +0530

    Data Fetching from input box:Done 1) Added logic in html and ts file

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -1,31 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef  } from '@angular/core';
 import { Router } from '@angular/router';
-
+ 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html'
 })
 export class LoginComponent {
-
+  @ViewChild('userName') userName: ElementRef;
+  @ViewChild('userPassword') userPassword: ElementRef;
+  
   constructor (private router:Router){
-
   }
+   
   ClickFunction(event) { 
 /**
  * == -> It will value only
  * === -> It will check value and datatype
  */
-    if (Username==="abc@abc.com" && Password==="abc") {
-      this.router.navigate(['/dashboard']);
-      
+
+    if (this.userName.nativeElement.value==="abc@abc.com" && this.userPassword.nativeElement.value==="abc") {
+      this.router.navigate(['/dashboard']);  
     } else {
       alert("Login Alert");
-    }
-
-   
-   
+    }   
     
-    console.log(event);
-    // alert("Login Alert");
-    // console.log(event);
  }}
